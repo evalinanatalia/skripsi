466eb07ae12398b871b153c5e122e9eaa8cf9f0d
vaa <evalina@clappingape.com>
2019-12-11 15:05:03 +0700

    fisrt commit

---
+++ app/src/main/java/com/example/trusts/MobilKeluarActivity.java
@@ -0,0 +1,13 @@
+package com.example.trusts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MobilKeluarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobil_keluar);
+    }
+}
